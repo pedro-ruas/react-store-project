f10c56499cbec0ec48566e8c4a8ee4d3d68800e7
Pedro Janelli da Silva Ruas <54222238+pedro-ruas@users.noreply.github.com>
2023-12-18 16:30:28 -0300

    RS-001, add categories and individualize components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-import './categories.styles.scss';
-
-const CATEGORIES = [
-  {
-    title: "Hats",
-    image: "",
-  },
-  {
-    title: "Jackets",
-    image: "",
-  },
-  {
-    title: "Sneakers",
-    image: "",
-  },
-  {
-    title: "Women",
-    image: "",
-  },
-  {
-    title: "Men",
-    image: "",
-  },
-];
+import { Categories } from "./components/categories/categories.component";
 
 function App() {
   return (
-    <div className="Categories-Container">
-      {CATEGORIES.map((category) => (
-        <div key={category.title} className="Category-Container">
-          <div className="Category-Background">
-            {/* <img src={category.image} /> */}
-          </div>
-          <div className="Category-Content">
-            <h2>{category.title}</h2>
-            <p>Show Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Categories />
   );
 }
 
---
+++ src/components/category/category.component.jsx
@@ -0,0 +1,18 @@
+import "./category.styles.scss";
+
+export function Category(props) {
+  const { title, image } = props;
+
+  return (
+    <div key={title} className="Category-Container">
+      <div
+        className="Category-Background"
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+      <div className="Category-Content">
+        <h2>{title}</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+  );
+}
