ae8241290ca671dcbd008cda187ec1bc1ca789b8
Pedro Janelli da Silva Ruas <54222238+pedro-ruas@users.noreply.github.com>
2023-12-18 15:29:16 -0300

    RS-001, create initial Homepage Sketch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import './categories.styles.scss';
+
+const CATEGORIES = [
+  {
+    title: "Hats",
+    image: "",
+  },
+  {
+    title: "Jackets",
+    image: "",
+  },
+  {
+    title: "Sneakers",
+    image: "",
+  },
+  {
+    title: "Women",
+    image: "",
+  },
+  {
+    title: "Men",
+    image: "",
+  },
+];
+
 function App() {
   return (
-    <div>
-      Hello Friends!
+    <div className="Categories-Container">
+      {CATEGORIES.map((category) => (
+        <div key={category.title} className="Category-Container">
+          <div className="Category-Background">
+            {/* <img src={category.image} /> */}
+          </div>
+          <div className="Category-Content">
+            <h2>{category.title}</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
